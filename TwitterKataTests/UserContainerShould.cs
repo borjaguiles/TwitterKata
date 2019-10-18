6d6125133b8f66cdc40bcf7c213929056ed1e318
Borja-Guiles <bguiles@voxelgroup.net>
2019-10-18 09:49:49 +0200

    test; Save many users then return them in a random order

--- TwitterKataTests/UserContainerShould.cs
+++ TwitterKataTests/UserContainerShould.cs
@@ -24,5 +24,25 @@ namespace TwitterKataTests
             //Assert
             Assert.Equal(expectedUser.IsMyName("Juan"), result.IsMyName("Juan"));
         }
+
+        [Fact]
+        public void SaveThreeUsersThenReturnThemInARandomOrderByName()
+        {
+            //Assemble
+            UserContainer userContainer = new UserContainer();
+            userContainer.AddNewUser("Juan");
+            userContainer.AddNewUser("Carlos");
+            userContainer.AddNewUser("Felipe");
+
+            //Act
+            var resultFirst = userContainer.GetUser("Juan");
+            var resultSec = userContainer.GetUser("Carlos");
+            var resultThird = userContainer.GetUser("Felipe");
+
+            //Assert
+            Assert.True(resultFirst.IsMyName("Juan"));
+            Assert.True(resultSec.IsMyName("Carlos"));
+            Assert.True(resultThird.IsMyName("Felipe"));
+        }
     }
 }
