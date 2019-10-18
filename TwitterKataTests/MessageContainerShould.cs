8a5fcf51aa7bf947e1f9e7d37743f59d3acdc4ed
Borja-Guiles <bguiles@voxelgroup.net>
2019-10-18 10:25:36 +0200

    test: AddAMessageAndReturnIt

--- TwitterKataTests/MessageContainerShould.cs
+++ TwitterKataTests/MessageContainerShould.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using TwitterKata.Messages;
+using Xunit;
 
 namespace TwitterKataTests
 {
-    class MessageContainerShould
+    public class MessageContainerShould
     {
+        [Fact]
+        public void AddAMessageAndReturnIt()
+        {
+            MessageContainer messageContainter = new MessageContainer();
+            messageContainter.AddMessage("Hola");
+
+            //Act
+            var result = messageContainter.GetMessagesAsText();
+
+            //Assert
+            Assert.Equal("Hola (0 seconds ago)", result.First());
+        }
     }
 }
